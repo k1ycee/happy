34b4ca82db554927cb78991f1efacb7e56fae4c8
Denzel <chiagozieani145@gmail.com>
2020-01-11 00:28:01 +0100

    like an app

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
           home: Scaffold(
             backgroundColor: Colors.white,
         appBar: AppBar(
