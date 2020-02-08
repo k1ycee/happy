d9d0abdd25af6fe3c6b90b58d1b44d7620755a48
Denzel <chiagozieani145@gmail.com>
2020-02-08 05:52:27 +0100

    Wow weee i think I'm done finish later

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class _SPFFState extends State<SPFF> {
           return Center(child: Text('No weather is available now'));
         }
         if (state is WeatherLoaded){
-          return Center(child: Text(state.weather.city.timezone.toString()),);
+          return Center(child: Text(state.weather.city.name.toString()));
         }
       },
      );
